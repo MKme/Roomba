fafae59ed27333d7382f78fd7301423dcf185403
Eric <info@mkme.org>
2018-12-10 22:44:29 -0500

    Codefix

--- RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
+++ RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
@@ -1,5 +1,10 @@
 /*
   A trivial Arduino sketch to mimic iRobot Virtual Wall for Roomba
+Youtube : http://www.youtube.com/mkmeorg Website, Forum and store are at http://mkme.org
+Parts needed
+Arduino NANO https://amzn.to/2EaMzZI
+IR LED https://amzn.to/2QOqUgE
+Resistor https://amzn.to/2EhIbsf
   ----------------------------------------------------------------
   Based on information found at:
   http://sites.google.com/site/irobotcreate2/createanirbeacon
