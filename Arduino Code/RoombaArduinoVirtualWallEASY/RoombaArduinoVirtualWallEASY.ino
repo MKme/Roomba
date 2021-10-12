4c502a73f70b6520347a9b4a9a9cbca29cacfa9f
David <CC2224@web.de>
2021-10-13 01:05:29 +0200

    Cleanup code

--- Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
+++ Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
@@ -9,14 +9,10 @@ Resistor https://amzn.to/2EhIbsf
   Based on information found at:
   http://sites.google.com/site/irobotcreate2/createanirbeacon
   
-  Uses "A Multi-Protocol Infrared Remote Library for the Arduino":
-  http://www.arcfn.com/2009/08/multi-protocol-infrared-remote-library.html
+  Uses https://github.com/Arduino-IRremote/Arduino-IRremote
 */
 
 #include <IRremote.h>
-
-// Initiate IRremote on Pin 3.
-//IRsend irsend(3);
 
 void setup()
 {
